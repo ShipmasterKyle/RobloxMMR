457654f62439d5036787a51abcdfe995a522fb8d
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-05-30 17:59:50 -0400

    Remove Archidemes Cache

--- src/ServerStorage/Archimedes_Storage/Main Building/Doorway/InsideDoor/Script.server.lua
+++
@@ -1,2 +0,0 @@
-script.Parent.Base.BodyGyro.cframe = script.Parent.Base.CFrame
-script.Parent.Base.Anchored = false
--- src/ServerStorage/Archimedes_Storage/Main Building/Trumpet/Script.server.lua
+++
@@ -1,9 +0,0 @@
-local tool = game.ServerStorage.Trumpet
-
-script.Parent.ClickDetector.MouseClick:Connect(function(plr)
-	if not plr.Backpack:FindFirstChild(tool.Name) and not plr:FindFirstChildWhichIsA("Tool") then
-		local myInstryument = tool:Clone()
-		myInstryument.Parent = plr.Backpack
-		print("Given.")
-	end
-end)
