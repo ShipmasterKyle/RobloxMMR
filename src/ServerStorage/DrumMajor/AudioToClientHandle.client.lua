a8678cef5cd6306b874d6d38f9477387079f5260
Ship <kylekates.gamin@gmail.com>
2022-06-25 22:16:02 -0400

    The Final Patch (OVA)
    You may know everything I'm gonna do but that won't help you because I know everything you're gonna do.
    
    STRANGE, ISN'T IT???

--- src/ServerStorage/DrumMajor/AudioToClientHandle.client.lua
+++ src/ServerStorage/DrumMajor/AudioToClientHandle.client.lua
@@ -1,35 +1,35 @@
---[[
-	AudioToClientHandle
-	Responsible for general sanity checks on audio.
-	Written by Drgametime3d (aka ShipmasterKyle) 2022. 
-	Do not steal.
-]]
+-- --[[
+-- 	AudioToClientHandle
+-- 	Responsible for general sanity checks on audio.
+-- 	Written by Drgametime3d (aka ShipmasterKyle) 2022. 
+-- 	Do not steal.
+-- ]]
 
---Table for the correct audios seen in the audio group.
-local correctAudio = {
-	A = {
-		Name = "Whistle",
-		ID = "rbxassetid://9774032026"
-	}
-}
+-- --Table for the correct audios seen in the audio group.
+-- local correctAudio = {
+-- 	A = {
+-- 		Name = "Whistle",
+-- 		ID = "rbxassetid://9774032026"
+-- 	}
+-- }
 
---The notes folder
-local notes = script.Parent.Notes
+-- --The notes folder
+-- local notes = script.Parent.Notes
 
-function find(array,item)
-	for i,v in pairs(array) do
-		if v.Name == item then
-			return v
-		else end
-	end
-	return false
-end
+-- function find(array,item)
+-- 	for i,v in pairs(array) do
+-- 		if v.Name == item then
+-- 			return v
+-- 		else end
+-- 	end
+-- 	return false
+-- end
 
-local RS = game:GetService("RunService")
-RS.Heartbeat:Connect(function()
-	for i,v in pairs(notes:GetChildren()) do
-		if find(correctAudio,v.Name) then
-			v.SoundId = find(correctAudio,v.Name).ID
-		end
-	end
-end)
+-- local RS = game:GetService("RunService")
+-- RS.Heartbeat:Connect(function()
+-- 	for i,v in pairs(notes:GetChildren()) do
+-- 		if find(correctAudio,v.Name) then
+-- 			v.SoundId = find(correctAudio,v.Name).ID
+-- 		end
+-- 	end
+-- end)
--- src/ServerStorage/Snare/AudioToServerHandle.server.lua
+++ src/ServerStorage/Snare/AudioToServerHandle.server.lua
@@ -1,39 +1,39 @@
---[[
-	AudioToServerHandle
-	Responsible for general sanity checks on audio.
-	Written by Drgametime3d (aka ShipmasterKyle) 2022. 
-	Do not steal.
-]]
+-- --[[
+-- 	AudioToServerHandle
+-- 	Responsible for general sanity checks on audio.
+-- 	Written by Drgametime3d (aka ShipmasterKyle) 2022. 
+-- 	Do not steal.
+-- ]]
 
---Table for the correct audios seen in the audio group.
-local correctAudio = {
-	A = {
-		Name = "Left",
-		ID = "rbxassetid://9744434475"
-	},
-	Ab = {
-		Name = "Right",
-		ID = "rbxassetid://9744434475"
-	}
-}
+-- --Table for the correct audios seen in the audio group.
+-- local correctAudio = {
+-- 	A = {
+-- 		Name = "Left",
+-- 		ID = "rbxassetid://9744434475"
+-- 	},
+-- 	Ab = {
+-- 		Name = "Right",
+-- 		ID = "rbxassetid://9744434475"
+-- 	}
+-- }
 
---The notes folder
-local notes = script.Parent.Notes
+-- --The notes folder
+-- local notes = script.Parent.Notes
 
-function find(array,item)
-	for i,v in pairs(array) do
-		if v.Name == item then
-			return v
-		else end
-	end
-	return false
-end
+-- function find(array,item)
+-- 	for i,v in pairs(array) do
+-- 		if v.Name == item then
+-- 			return v
+-- 		else end
+-- 	end
+-- 	return false
+-- end
 
-local RS = game:GetService("RunService")
-RS.Heartbeat:Connect(function()
-	for i,v in pairs(notes:GetChildren()) do
-		if find(correctAudio,v.Name) then
-			v.SoundId = find(correctAudio,v.Name).ID
-		end
-	end
-end)
+-- local RS = game:GetService("RunService")
+-- RS.Heartbeat:Connect(function()
+-- 	for i,v in pairs(notes:GetChildren()) do
+-- 		if find(correctAudio,v.Name) then
+-- 			v.SoundId = find(correctAudio,v.Name).ID
+-- 		end
+-- 	end
+-- end)
