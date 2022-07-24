a929639219f120e131671e5ffcf0a8ac8209aeb8
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-07-24 14:29:37 -0400

    Percussion Patches

--- src/ServerStorage/BassDrum/WeldToServ.server.lua
+++ src/ServerStorage/BassDrum/WeldToServ.server.lua
@@ -3,15 +3,21 @@ local eva = script.Parent.CleanUp
 
 eve.OnServerEvent:Connect(function(plr)
 	local char = plr.Character
-	if char then
+	if char and char:FindFirstChild("LeftHand") then
 		local weld1 = Instance.new("WeldConstraint")
 		local weld2 = Instance.new("WeldConstraint")
 		local humroot = char.HumanoidRootPart
 		local leftHand = char.LeftHand
 		local snare = script.Parent.BassDrum
 		local stick = script.Parent.LeftStick
-		snare:SetPrimaryPartCFrame(humroot.CFrame + Vector3.new(0,-1,2))
-		weld1.Part0 = snare.PrimaryPart
+		--Fix the Anchor Problem
+		for i,v in pairs(snare:GetDescendants())
+			if v:IsA("BasePart") then
+				v.Anchored = false
+			end
+		end
+		snare.CFrame = humroot.CFrame + Vector3.new(0,-1,2)
+		weld1.Part0 = snare --> This is a part now
 		weld1.Part1 = humroot
 		weld1.Parent = snare
 		weld1.Name = "Weld1"
