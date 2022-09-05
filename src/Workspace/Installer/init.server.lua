818a25072c877567d14b1d328a1e9cb5723a48e3
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-09-05 14:32:16 -0400

    Audio should be replicated over the server

--- src/Workspace/Installer/init.server.lua
+++ src/Workspace/Installer/init.server.lua
@@ -10,7 +10,8 @@ local mainMessage = [[
 	This kit allows your band members to actaully play their instruments.
 	No more using codes!
 
-    As of v7.25.22 Adonis is a dependency of this kit. As such it is included with it
+    As of v7.25.22 Adonis is a dependency of this kit. As such it is included with it.
+	As if v9.8.22 the kit will require that you set SoundService.RespectFilteringEnabled to false to reduce lag.
 	
 	This script and all the script you'll see in this kit (excluding Adonis) was written by ShipmasterKyle.
 	Send the main place id of your band room to ShipmasterKyle#8071 and make sure you give SubToShip developer access to your hall.
@@ -88,45 +89,56 @@ mainBox.Parent = frame
 nextbtn.Parent = frame
 
 nextbtn.MouseButton1Click:Connect(function()
-	if models then
-		nextbtn.Visible = false
-		mainBox.Text = "Please Wait..."
-		for i,v in pairs(models:GetChildren()) do
-			if v.Name == "Adonis_Loader" then
-				if not workspace:FindFirstChild(v.Name) then
-					v.Parent = workspace
+	if SoundService.RespectFilteringEnabled == false then
+		if models then
+			nextbtn.Visible = false
+			mainBox.Text = "Please Wait..."
+			for i,v in pairs(models:GetChildren()) do
+				if v.Name == "Adonis_Loader" then
+					if not workspace:FindFirstChild(v.Name) then
+						v.Parent = workspace
+					end
+				else
+					if game.ServerStorage:FindFirstChild(v.Name) then
+						game.ServerStorage[v.Name]:Destroy()
+					end
+					v.Parent = game.ServerStorage
+					wait(0.0125)
 				end
-			else
-				if game.ServerStorage:FindFirstChild(v.Name) then
-					game.ServerStorage[v.Name]:Destroy()
-				end
-				v.Parent = game.ServerStorage
-				wait(0.0125)
 			end
-		end
-		-- local remote = Instance.new("RemoteEvent")
-		-- remote.Name = "March"
-		-- remote.Parent = game.ReplicatedStorage
-		mainBox.Text = "All Done! Enjoy! You can delete the Installer Script in workspace."
-		newbtn.Text = "Close"
-		newbtn.Visible = true
-	else
-		nextbtn.Visible = false
-		mainBox.Text = "Models Not Found, Attempting to build from scratch."
-		wait(1)
-		local instruments = game.ServerStorage:GetDescendants()
-		if #instruments ~= 0 or not game.ServerStorage:FindFirstOfClass("Tool") then --Check for the Instruments scripts.
-			mainBox.Text = [[No recoverable scripts founds.]]..ErrorMessage
+			-- local remote = Instance.new("RemoteEvent")
+			-- remote.Name = "March"
+			-- remote.Parent = game.ReplicatedStorage
+			mainBox.Text = "All Done! Enjoy! You can delete the Installer Script in workspace."
+			newbtn.Text = "Close"
+			newbtn.Visible = true
 		else
-			mainBox.Text = "Scripts Found! Attempting to rebuild. This may take a few minutes"
-			for i,v in pairs(game.ServerStorage:GetDescendants()) do
-				if v.ClassName == "Tool" then
-					if table.find(brass, v.Name) then
-						print("Coming Soon")
+			nextbtn.Visible = false
+			mainBox.Text = "Models Not Found, Attempting to build from scratch."
+			wait(1)
+			local instruments = game.ServerStorage:GetDescendants()
+			if #instruments ~= 0 or not game.ServerStorage:FindFirstOfClass("Tool") then --Check for the Instruments scripts.
+				mainBox.Text = [[No recoverable scripts founds.]]..ErrorMessage
+			else
+				mainBox.Text = "Scripts Found! Attempting to rebuild. This may take a few minutes"
+				for i,v in pairs(game.ServerStorage:GetDescendants()) do
+					if v.ClassName == "Tool" then
+						if table.find(brass, v.Name) then
+							print("Coming Soon")
+						end
 					end
 				end
 			end
 		end
+	else
+		warn("SoundService.RespectFilteringEnabled needs to be disabled to continue!")
+		mainBox.Text = [[
+			Dependency Error
+
+			You need to go to SoundService in the explorer and then in properties set RespectFilteringEnabled to false.
+
+			This must be done manuaully as the property can not be editted by the installer.
+		]]		
 	end
 end)
 
